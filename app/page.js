19d89a5c4fab9493ec7f77af2efe7f6d845c9465
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-07-18 10:48:46 +0300

    add form to page

--- app/page.js
+++ app/page.js
@@ -1,7 +1,17 @@
 export default function Home() {
   return (
     <main className="bg-green-300 flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Jesus did it again</h1>
+      <form className="bg-white rounded-lg flex w-1/2">
+        <div className="flex-1 text-gray-700 p-20">
+          <h1 className="text-3xl pb-2 font-latoBold">
+            Let's get started 👏
+          </h1>
+          <p className="text-lg  text-gray-500">
+            Join our E-learning platform today and unlock over 500+ courses
+            and digital assets ready to download.
+          </p>
+        </div>
+      </form>
     </main>
   );
 }
