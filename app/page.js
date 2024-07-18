028c576ecd1b867900a939c0bfc72a09787786c0
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-07-18 11:28:36 +0300

    edit page js

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ export default function Home() {
     <main className="bg-green-300 flex min-h-screen flex-col items-center justify-between p-24">
       <form className="bg-white rounded-lg flex w-1/2">
         <div className="flex-1 text-gray-700 p-20">
-          <h1 className="text-3xl pb-2 font-latoBold">Let's get started 👏</h1>
+          <h1 className="text-3xl pb-2 font-latoBold">Lets get started 👏</h1>
           <p className="text-lg  text-gray-500">
             Join our E-learning platform today and unlock over 500+ courses and
             digital assets ready to download.
