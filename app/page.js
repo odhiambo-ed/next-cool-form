ebeac37e2e3cae015ee02d42dc4df967060fb3c0
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-07-18 11:36:11 +0300

    edit page js

--- app/page.js
+++ app/page.js
@@ -1,41 +1,105 @@
+import formImage from '../public/dog.png'
+import Image from 'next/image';
+
 export default function Home() {
   return (
     <main className="bg-green-300 flex min-h-screen flex-col items-center justify-between p-24">
-      <form className="bg-white rounded-lg flex w-1/2">
-        <div className="flex-1 text-gray-700 p-20">
-          <h1 className="text-3xl pb-2 font-latoBold">Lets get started 👏</h1>
+      <form
+        className="bg-white flex rounded-lg w-1/2 font-latoRegular"
+      >
+        <div className="flex-1 text-gray-700  p-20">
+          <h1 className="text-3xl pb-2 font-latoBold">
+            Lets get started 👋
+          </h1>
           <p className="text-lg  text-gray-500">
-            Join our E-learning platform today and unlock over 500+ courses and
-            digital assets ready to download.
+            Join our E-learning platform today and unlock over 500+ courses
+            and digital assets ready to download.
           </p>
-          <div className="mt-6">
+          <div className="mt-6 ">
             <div className="pb-4">
               <label
                 htmlFor="name"
-                className="block text-sm pb-2 font-latoBold"
+                className="block font-latoBold text-sm pb-2"
               >
                 Name
               </label>
+              <p className="text-sm font-latoBold text-red-400 "></p>
               <input
+                className="border-2 border-gray-500 p-2 rounded-md w-1/2 focus:border-teal-500 focus:ring-teal-500 "
                 type="text"
                 name="name"
-                className="border-2 border-gray-500 p-2 rounded-md w-1/2 focus:border-teal-500 focus:ring-teal-500"
-                placeholder="Enter you name"
+                placeholder="Enter your name"
               />
             </div>
             <div className="pb-4">
               <label
-                htmlFor=""
+                htmlFor="email"
                 className="block font-latoBold text-sm pb-2"
               >
                 Email
               </label>
+
+              <p></p>
               <input
-                type="text"
                 className="border-2 border-gray-500 p-2 rounded-md w-1/2 focus:border-teal-500 focus:ring-teal-500"
+                type="email"
+                name="email"
+                placeholder="Enter your email address"
               />
             </div>
+            <div className="pb-4">
+              <label
+                htmlFor="country"
+                className="block font-latoBold text-sm pb-2"
+              >
+                Country
+              </label>
+              <select
+                className="border-2 border-gray-500 p-2 rounded-md w-1/2 focus:border-teal-500 focus:ring-teal-500"
+                name="country"
+              >
+                <option>United States</option>
+                <option>United Kingdom</option>
+                <option>Germany</option>
+              </select>
+            </div>
+            <div className="pb-4">
+              <label
+                htmlFor="terms"
+                className="block font-latoBold text-sm pb-2"
+              >
+                Terms of service
+              </label>
+
+              <div className="flex items-center gap-2">
+                <input
+                  type="checkbox"
+                  name="terms"
+                  value="checked"
+                  className="h-5 w-5 text-teal-500 border-2  background-gray-500 focus:border-teal-500 focus:ring-teal-500"
+                />
+                <p className="text-sm font-latoBold text-gray-500">
+                  I agree to the Terms and Service that my data will be taken
+                  and sold.
+                </p>
+              </div>
+            </div>
+            <button
+              type="submit"
+              className="bg-teal-500 font-latoBold text-sm text-white py-3 mt-6 rounded-lg w-full"
+            >
+              Start learning today!
+            </button>
           </div>
+        </div>
+        <div className="relative flex-1">
+          <Image
+            className=" object-cover rounded-lg"
+            fill
+            priority
+            src={formImage}
+            alt="form-learn"
+          />
         </div>
       </form>
     </main>
