b2e6ce3e0affb9a7abcc43024c9b62036bf011b7
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-07-18 10:33:49 +0300

    edit pages

--- app/page.js
+++ app/page.js
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="bg-green-300 flex min-h-screen flex-col items-center justify-between p-24">
       <h1>Jesus did it again</h1>
     </main>
   );
